ca3387f36a66db25d8a72701a43cccc8322bf5f5
bysq <1904568840@qq.com>
2024-11-13 21:48:22 +0800

    666

--- 作业.py
+++ 作业.py
@@ -1,9 +1,9 @@
-# import random
-# rand_number:int = random.randint(1,100)
-# number:int = eval(input("猜:")) 
-# if number > rand_number:
-#     print("大了。")
-# elif number < rand_number:
-#     print("小了。")
-# else:
-#     print("对了。")
+import random
+rand_number:int = random.randint(1,100)
+number:int = eval(input("猜:")) 
+if number > rand_number:
+    print("大了。")
+elif number < rand_number:
+    print("小了。")
+else:
+    print("对了。")
