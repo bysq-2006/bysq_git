16108c9c0b339661c788b4335fc7525043062b98
bysq <1904568840@qq.com>
2024-11-13 22:24:08 +0800

    没什么用

--- 作业.py
+++
@@ -1,9 +0,0 @@
-import random
-rand_number:int = random.randint(1,100)
-number:int = eval(input("猜:")) 
-if number > rand_number:
-    print("大了。")
-elif number < rand_number:
-    print("小了。")
-else:
-    print("对了。")
