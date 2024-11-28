a928d8cc45ea9f55fd07d6c0a30649b88d498b43
bysq <1904568840@qq.com>
2024-11-28 20:47:41 +0800

    web

--- 作业.py
+++ 作业.py
@@ -1,12 +1,5 @@
-def num_pd(number:int) -> bool:
-    sum:int = 0
-    for i in range(1,number,1):
-        if number % i == 0:
-            sum += i
-    if sum == number:
-        return True
-    else:
-        return False
-
-num = eval(input())
-print(f"{num} is {num_pd(num)}")
+for i in range(7,0,-1):
+    print(' '*(7-i)*3,end='')
+    for j in range(1,i,1):
+        print(j,end='  ')
+    print('')
